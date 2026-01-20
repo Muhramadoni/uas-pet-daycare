20c353ea2e2605366f00c90c4a70cd2cd0ecba49
Muhramadoni <147119106+Muhramadoni@users.noreply.github.com>
2026-01-20 18:36:03 +0700

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiUrl =
-  import.meta.env.VITE_API_URL || "https://uas-pet-daycare-rwie.vercel.app/";
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const instance = axios.create({
   baseURL: apiUrl,
